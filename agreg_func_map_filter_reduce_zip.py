1d89008b2cc0ab795c903363344db6e5068e0de5
alexmudra <ko9#ba9$>
2022-08-26 16:01:09 +0300

    added example of filter function

--- agreg_func_map_filter_reduce_zip.py
+++ agreg_func_map_filter_reduce_zip.py
@@ -1,4 +1,6 @@
-#м map - приймає як параметр фукцію як обєкт
+import requests
+
+# map function - приймає як параметр фукцію як обєкт
 #приклад:
 # def go_map (a):
 #     return a**2
@@ -7,10 +9,28 @@
 # print(list(do_map)) #результат print(list(do_map))
 
 ###################### 2 #############################
+def f (a, b):
+    return a * b
 
-import requests
+lst_one = [1, 2, 3]
+lst_two = [4, 5, 6]
+lst_tree = [7, 8]
 
+use_map = map(f, lst_one, lst_two) #перемножиться кожне значення ліста1 і ліста2 по черзі
+use_map = map(f, [1, 2, 3, 4], [5, 6, 7, 8]) #перемножиться кожне значення із вложених  лістів аргументів
 
+use_map_g = map(f, lst_one, lst_tree) #перемножиться лиле 2 значення із вложених  лістів аргументів
+print(f"Result of 2 numbers {list(use_map_g)}")  #Result of 2 numbers [7, 16]
+
+print(use_map) #поверне обєкт <map object at 0x01433FB8>
+print(list(use_map)) #конвертуємо перемінну в ліст і виведе результат множення відповідно до f фукції
+
+###################### 2 #############################
+#використаємо кортеж і лямбда фуцкцію
+d = map(lambda x:x+15, (1,2,3,4)) #додамо 15 до кожного значення із кортежа
+print(list(d)) #res [16, 17, 18, 19]
+
+###################### 3 #############################
 def capital_lettered_quotes():
     quotes = requests.get(
         "https://raw.githubusercontent.com/ranjith19/random-quotes-generator/master/quotes.txt").text.split("\n.\n") #взяли текст методом GET і розділили його по пробілу
@@ -21,3 +41,10 @@ def capital_lettered_quotes():
     print( {"initial": quotes, "capitalized": quotes_capital})
 
 capital_lettered_quotes()
+
+###################### reduce function #############################
+#reduce фуцкція повертає лише одне значення із послідовності яке відповідає логічній умові
+from functools import reduce
+
+j = reduce(lambda x, b: x * b, (1, 2, 3, 4, 50, 6))
+print(j)
--- filter_functions.py
+++ filter_functions.py
@@ -1,35 +1,49 @@
+# це фукція яка використовується для фільтрації елементів послідовності: отримує фукцію яка повертаж True/False і послідовність яку потрібно фільтрувати
 # Simple usage of filter function
 # Filtering quotes leaving only that, which contains word or phrase given in the input
 
-filter_template = """
-<table>
-<tr><th>Found with pattern '{}'</th></tr>
-{}
-<tr><td><hr><br>Found {} occurances</td></tr>
-</table>
-"""
-
-import requests
-
-
-def filter_quotes(pattern: str) -> list:
-    quotes = requests.get(
-        "https://raw.githubusercontent.com/ranjith19/random-quotes-generator/master/quotes.txt").text.split("\n.\n")
-
-    return list(filter(lambda quote: pattern in quote, quotes))
-
-
-from IPython.display import HTML
-
-filtered = ""
-pattern = input("What to search between the quotes? ")
-
-filtered_quotes = filter_quotes(pattern)
-
-for quote in filtered_quotes:
-    filtered += f"<tr><td>{quote}</td></tr>"
-
-display(HTML(filter_template.format(pattern, filtered, len(filtered_quotes))))
+# filter_template = """
+# <table>
+# <tr><th>Found with pattern '{}'</th></tr>
+# {}
+# <tr><td><hr><br>Found {} occurances</td></tr>
+# </table>
+# """
+#
+# import requests
+#
+#
+# def filter_quotes(pattern: str) -> list:
+#     quotes = requests.get(
+#         "https://raw.githubusercontent.com/ranjith19/random-quotes-generator/master/quotes.txt").text.split("\n.\n")
+#
+#     return list(filter(lambda quote: pattern in quote, quotes))
+#
+#
+# from IPython.display import HTML
+#
+# filtered = ""
+# pattern = input("What to search between the quotes? ")
+#
+# filtered_quotes = filter_quotes(pattern)
+#
+# for quote in filtered_quotes:
+#     filtered += f"<tr><td>{quote}</td></tr>"
+#
+# display(HTML(filter_template.format(pattern, filtered, len(filtered_quotes))))
 
 #   код не буде працювати, тому що display(HTML(filter_template.format(pattern, filtered, len(filtered_quotes))))
 # NameError: name 'display' is not defined
+
+#### 3 #### фукція яка повертає лише парні числа
+def s (a):
+    if a % 2 == 0:
+        return a
+
+k = filter(s, (1,2,3,4)) #додамо 15 до кожного значення із кортежа
+print(list(k)) #res [16, 17, 18, 19]
+
+#фукція яка повертає лише парні числа тільки через лямбда
+l = filter(lambda x: (x % 2 == 0), (2, 3, 4, 5, 6, 10, 20, 15))
+print(list(l)) #res [2, 4, 6, 10, 20]
+
