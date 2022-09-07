56af6f94154d32b43587cf67083240a3f9b0c174
alexmudra <ko9#ba9$>
2022-09-07 07:59:03 +0300

    working example with uk -> en via GOOGLE translator module 2

--- translator.py
+++ translator.py
@@ -1,5 +1,4 @@
 #https://dev-gang.ru/article/perevod-teksta-s-pomosczu-google-translate-api-v-python-ahgm88wx1k/
-
 from googletrans import Translator
 
 translator = Translator()
@@ -9,9 +8,24 @@ print(f"Текст після перекладу в нашому випадку 
 print(f"Мова на яку потрібно перекластиє: {translation_rslt.dest}")
 print(f"Вимова буде така: {translation_rslt.pronunciation}")
 
+##############################
+transl_phrase ="Привіт світ"
+translation_rslt_to_ukr = translator.translate(transl_phrase, src='uk',dest='en')
+print(f"фраза/слово uk: {transl_phrase} -> переклад на en: {translation_rslt_to_ukr.text}") #фраза/слово uk: Привіт світ -> переклад на en: Hi world
+##############################
+#переклад сліів/фраз із списку
+uk_phrases = ['закупівлі', 'допорогова закупівля', 'учасник', 'номер тендера']
+tran_ukr_phrs_rslt_to_en = translator.translate(uk_phrases, src='uk', dest='en')
+for trnsl in tran_ukr_phrs_rslt_to_en:
+    print(trnsl.text)
 
-translation_rslt_to_ukr = translator.translate("Привіт",src='uk',dest='en')
-print(translation_rslt_to_ukr.text) #Hello
+
+
+
+
+
+
+
 
 
 # quotes_file = open(r"C:\Users\alex\PycharmProjects\automation_with_cb\quotes.txt")
